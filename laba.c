bfae49991935a9801480a054c547ec2bf0df50c4
Nikita-Lazurchenko <nikitalazurcenko@gmail.com>
2024-12-16 18:28:29 +0300

    project with variable blinking lights (laba_first)

--- laba.c
+++ laba.c
@@ -1,35 +1,85 @@
-#include<stdio.h>
+/* STM32 Blue Pill: Blink an LED on PB9 */
 
-typedef struct {
-    char *name;       
-    char *core;      
-    int  frequency; 
-    int  temperature;
-} microcontroller;
+#include "stm32f1xx_hal.h"
 
-int main() {
-    microcontroller STM32F407VG = {
-        .name = "STM32F407VG",
-        .core = "Cortex-M4F",
-        .frequency = 125, 
-        .temperature = 45
-    };
+#define LED1_PIN                               GPIO_PIN_9
+#define LED2_PIN                               GPIO_PIN_8
+#define LED3_PIN                               GPIO_PIN_7
+#define LED4_PIN                               GPIO_PIN_6
+#define LED_GPIO_PORT                          GPIOB
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOB_CLK_ENABLE()
+
+int main(void)
+{
+  HAL_Init();
+
+  LED_GPIO_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = LED1_PIN | LED2_PIN | LED3_PIN | LED4_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+
+  while (1)
+  {
+      HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
+      HAL_Delay(500);
+      HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
+      HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
+      HAL_Delay(500);
+      HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
+      HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
+      HAL_Delay(500);
+      HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
+      HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN);
+      HAL_Delay(500);
+      HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN);
+
+  }
+}
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+void NMI_Handler(void)
+{
+}
+
+void HardFault_Handler(void)
+{
+  while (1) {}
+}
 
 
-    printf("До модернизации:\n");
-    printf("Название: %s\n", STM32F407VG.name);
-    printf("Ядро: %s\n", STM32F407VG.core);
-    printf("Частота: %d МГц\n", STM32F407VG.frequency);
-    printf("Температура: %d C\n\n", STM32F407VG.temperature);
+void MemManage_Handler(void)
+{
+  while (1) {}
+}
 
-    STM32F407VG.frequency = 168;
-    STM32F407VG.temperature = 75;
+void BusFault_Handler(void)
+{
+  while (1) {}
+}
 
-    printf("После модернизации:\n");
-    printf("Название: %s\n", STM32F407VG.name);
-    printf("Ядро: %s\n", STM32F407VG.core);
-    printf("Частота: %d МГц\n", STM32F407VG.frequency);
-    printf("Температура: %d C\n", STM32F407VG.temperature);
+void UsageFault_Handler(void)
+{
+  while (1) {}
+}
 
-    return 0;
+void SVC_Handler(void)
+{
+}
+
+
+void DebugMon_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
 }
