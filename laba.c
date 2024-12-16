2691a0d76a7f76378b292219e2388d1160235cf0
Nikita-Lazurchenko <nikitalazurcenko@gmail.com>
2024-12-16 18:34:16 +0300

    implemented several frequency modes for switching LEDs using button (laba_first)

--- laba.c
+++ laba.c
@@ -6,9 +6,15 @@
 #define LED2_PIN                               GPIO_PIN_8
 #define LED3_PIN                               GPIO_PIN_7
 #define LED4_PIN                               GPIO_PIN_6
+
 #define LED5_PIN                               GPIO_PIN_5
+
+
+
 #define LED_GPIO_PORT                          GPIOB
-#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOB_CLK_ENABLE()
+
+
+#define LED_GPIO_CLK_ENABLE()                 __HAL_RCC_GPIOB_CLK_ENABLE()
 
 int main(void)
 {
@@ -24,28 +30,33 @@ int main(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
 
+  
   GPIO_InitStruct.Pin = LED5_PIN;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  while (1)
-  {
-      if(HAL_GPIO_ReadPin(LED_GPIO_PORT,LED5_PIN) == GPIO_PIN_SET){
+
+  long frequency = 1000;  
+  
+  while (1){
+    if(HAL_GPIO_ReadPin(LED_GPIO_PORT,LED5_PIN) == GPIO_PIN_SET){
         HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
-        HAL_Delay(500);
+        HAL_Delay(frequency);
         HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
         HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
-        HAL_Delay(500);
+        HAL_Delay(frequency);
         HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
         HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
-        HAL_Delay(500);
+        HAL_Delay(frequency);
         HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
         HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN);
-        HAL_Delay(500);
-        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN);
-      }
+        HAL_Delay(frequency);
+        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN); 
+
+        frequency = frequency == 100 ? 1000 : frequency - 100;
+    }
   }
 }
 
