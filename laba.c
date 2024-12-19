e62d4d9b5cba4e93a44e117b6549339741267ce8
Nikita-Lazurchenko <nikitalazurcenko@gmail.com>
2024-12-19 15:35:33 +0300

    modernized the code so that the delay is less (laba_second)

--- laba.c
+++ laba.c
@@ -1,4 +1,4 @@
-#include "stm32f1xx.h" 
+#include "stm32f1xx_hal.h"
 
 #define LED1_PIN  GPIO_PIN_9
 #define LED2_PIN  GPIO_PIN_8
@@ -7,6 +7,16 @@
 
 #define LED_PORT GPIOB
 
+#define TIMER_PRESCALER 72  
+#define TIMER_PERIOD 1000  
+
+volatile uint32_t timer_tick = 0;
+
+void SysTick_Handler(void) {
+    timer_tick++;
+}
+
+
 void delay_ms(uint32_t ms) {
   for (uint32_t i = 0; i < ms * 1000; i++) {
       __NOP();
@@ -14,23 +24,28 @@ void delay_ms(uint32_t ms) {
 }
 
 int main(void) {
-  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 
-  GPIO_InitTypeDef GPIO_InitStruct;
-  GPIO_InitStruct.Pin = LED1_PIN | LED2_PIN | LED3_PIN | LED4_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
-  while (1) {
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.Pin = LED1_PIN | LED2_PIN | LED3_PIN | LED4_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+
+    SysTick_Config(SystemCoreClock / TIMER_PRESCALER); 
+    
+    NVIC_SetPriority(SysTick_IRQn, 1);
+   
+    while (1) {
       HAL_GPIO_TogglePin(LED_PORT, LED1_PIN);
-      delay_ms(500);
+      delay_ms(100);
       HAL_GPIO_TogglePin(LED_PORT, LED2_PIN);
-      delay_ms(500);
+      delay_ms(100);
       HAL_GPIO_TogglePin(LED_PORT, LED3_PIN);
-      delay_ms(500);
+      delay_ms(100);
       HAL_GPIO_TogglePin(LED_PORT, LED4_PIN);
-      delay_ms(500);
-
-  }
+      delay_ms(100);
+    }
 }
