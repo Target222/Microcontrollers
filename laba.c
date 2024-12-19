794f2eebdd382d1c0b32f7ad23cf997daa4c0002
Nikita-Lazurchenko <nikitalazurcenko@gmail.com>
2024-12-19 15:22:22 +0300

    rewrote the LED lighting code for the CMSIS library (laba_second)

--- laba.c
+++ laba.c
@@ -1,117 +1,36 @@
-/* STM32 Blue Pill: Blink an LED on PB9 */
+#include "stm32f1xx.h" 
 
-#include "stm32f1xx_hal.h"
+#define LED1_PIN  GPIO_PIN_9
+#define LED2_PIN  GPIO_PIN_8
+#define LED3_PIN  GPIO_PIN_7
+#define LED4_PIN  GPIO_PIN_6
 
-#define LED1_PIN                               GPIO_PIN_9
-#define LED2_PIN                               GPIO_PIN_8
-#define LED3_PIN                               GPIO_PIN_7
-#define LED4_PIN                               GPIO_PIN_6
+#define LED_PORT GPIOB
 
-#define LED5_PIN                               GPIO_PIN_5
+void delay_ms(uint32_t ms) {
+  for (uint32_t i = 0; i < ms * 1000; i++) {
+      __NOP();
+    }
+}
 
-
-
-#define LED_GPIO_PORT                          GPIOB
-
-
-#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOB_CLK_ENABLE()
-
-int main(void)
-{
-  HAL_Init();
-
-  LED_GPIO_CLK_ENABLE();
+int main(void) {
+  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 
   GPIO_InitTypeDef GPIO_InitStruct;
-
   GPIO_InitStruct.Pin = LED1_PIN | LED2_PIN | LED3_PIN | LED4_PIN;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+  HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
 
-  
-  GPIO_InitStruct.Pin = LED5_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  while (1) {
+      HAL_GPIO_TogglePin(LED_PORT, LED1_PIN);
+      delay_ms(500);
+      HAL_GPIO_TogglePin(LED_PORT, LED2_PIN);
+      delay_ms(500);
+      HAL_GPIO_TogglePin(LED_PORT, LED3_PIN);
+      delay_ms(500);
+      HAL_GPIO_TogglePin(LED_PORT, LED4_PIN);
+      delay_ms(500);
 
-
-  long number_of_click = 0;   
-  while (1){
-    if(HAL_GPIO_ReadPin(LED_GPIO_PORT,LED5_PIN) == GPIO_PIN_SET){
-      if(number_of_click % 2 == 0){
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
-          HAL_Delay(500);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
-          HAL_Delay(500);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
-          HAL_Delay(500);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN);
-          HAL_Delay(500);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN); 
-      }else{
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN);
-          HAL_Delay(500);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED4_PIN);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
-          HAL_Delay(500);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED3_PIN);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
-          HAL_Delay(500);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED2_PIN);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN);
-          HAL_Delay(500);
-          HAL_GPIO_TogglePin(LED_GPIO_PORT, LED1_PIN); 
-      }
-      number_of_click++;
-    }
   }
-}
-
-void SysTick_Handler(void)
-{
-  HAL_IncTick();
-}
-
-void NMI_Handler(void)
-{
-}
-
-void HardFault_Handler(void)
-{
-  while (1) {}
-}
-
-
-void MemManage_Handler(void)
-{
-  while (1) {}
-}
-
-void BusFault_Handler(void)
-{
-  while (1) {}
-}
-
-void UsageFault_Handler(void)
-{
-  while (1) {}
-}
-
-void SVC_Handler(void)
-{
-}
-
-
-void DebugMon_Handler(void)
-{
-}
-
-void PendSV_Handler(void)
-{
 }
