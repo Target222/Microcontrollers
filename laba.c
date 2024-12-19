6adf4575bb2ed4a84f2e8975f29b1f351fd5df9b
Nikita-Lazurchenko <nikitalazurcenko@gmail.com>
2024-12-19 19:20:01 +0300

    the LEDs light up only after you click the button (laba_second)

--- laba.c
+++ laba.c
@@ -7,8 +7,11 @@
 
 #define LED_PORT GPIOB
 
-#define TIMER_PRESCALER 72  
-#define TIMER_PERIOD 1000  
+#define BUTTON_PIN GPIO_PIN_5
+#define BUTTON_PORT GPIOA
+
+#define TIMER_PRESCALER 72
+#define TIMER_PERIOD 1000
 
 volatile uint32_t timer_tick = 0;
 
@@ -16,17 +19,15 @@ void SysTick_Handler(void) {
     timer_tick++;
 }
 
-
 void delay_ms(uint32_t ms) {
-  for (uint32_t i = 0; i < ms * 1000; i++) {
-      __NOP();
+    for (uint32_t i = 0; i < ms * 1000; i++) {
+        __NOP();
     }
 }
 
 int main(void) {
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-
-    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 
     GPIO_InitTypeDef GPIO_InitStruct;
     GPIO_InitStruct.Pin = LED1_PIN | LED2_PIN | LED3_PIN | LED4_PIN;
@@ -34,18 +35,24 @@ int main(void) {
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
 
-    SysTick_Config(SystemCoreClock / TIMER_PRESCALER); 
-    
+    GPIO_InitStruct.Pin = BUTTON_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(BUTTON_PORT, &GPIO_InitStruct);
+
+    SysTick_Config(SystemCoreClock / TIMER_PRESCALER);
     NVIC_SetPriority(SysTick_IRQn, 1);
-   
+
     while (1) {
-      HAL_GPIO_TogglePin(LED_PORT, LED1_PIN);
-      delay_ms(100);
-      HAL_GPIO_TogglePin(LED_PORT, LED2_PIN);
-      delay_ms(100);
-      HAL_GPIO_TogglePin(LED_PORT, LED3_PIN);
-      delay_ms(100);
-      HAL_GPIO_TogglePin(LED_PORT, LED4_PIN);
-      delay_ms(100);
+         if (HAL_GPIO_ReadPin(BUTTON_PORT,BUTTON_PIN) == GPIO_PIN_SET) {
+             HAL_GPIO_TogglePin(LED_PORT, LED1_PIN);
+             delay_ms(100);
+             HAL_GPIO_TogglePin(LED_PORT, LED2_PIN);
+             delay_ms(100);
+             HAL_GPIO_TogglePin(LED_PORT, LED3_PIN);
+             delay_ms(100);
+             HAL_GPIO_TogglePin(LED_PORT, LED4_PIN);
+             delay_ms(100);
+        }
     }
 }
